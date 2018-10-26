bb14a8387791747eac7ebfdffe99494e133ad772
Adonayg <adonay@tutanota.com>
2018-10-26 13:24:58 +0100

    front end

--- src/main/webapp/table_script.js
+++ src/main/webapp/table_script.js
@@ -2,27 +2,27 @@ function edit_row(no) {
     document.getElementById("edit_button" + no).style.display = "none";
     document.getElementById("save_button" + no).style.display = "block";
 
-    var fname = document.getElementById("fname_row" + no);
-    var lname = document.getElementById("lname_row" + no);
-    var anum = document.getElementById("anum_row" + no);
+    var title = document.getElementById("title_row" + no);
+    var genre = document.getElementById("genre_row" + no);
+    var rating = document.getElementById("rating_row" + no);
 
-    var fname_data = name.innerHTML;
-    var lname_data = country.innerHTML;
-    var anum_data = age.innerHTML;
+    var title_data = name.innerHTML;
+    var genre_data = country.innerHTML;
+    var rating_data = age.innerHTML;
 
-    name.innerHTML = "<input type='text' id='fname_text" + no + "' value='" + name_data + "'>";
-    country.innerHTML = "<input type='text' id='lname_text" + no + "' value='" + lname_data + "'>";
-    age.innerHTML = "<input type='text' id='anum_text" + no + "' value='" + anum_data + "'>";
+    name.innerHTML = "<input type='text' id='title_text" + no + "' value='" + name_data + "'>";
+    country.innerHTML = "<input type='text' id='genre_text" + no + "' value='" + genre_data + "'>";
+    age.innerHTML = "<input type='text' id='rating_text" + no + "' value='" + rating_data + "'>";
 }
 
 function save_row(no) {
-    var fname_val = document.getElementById("fname_text" + no).value;
-    var lname_val = document.getElementById("lname_text" + no).value;
-    var anum_val = document.getElementById("anum_text" + no).value;
+    var title_val = document.getElementById("title_text" + no).value;
+    var genre_val = document.getElementById("genre_text" + no).value;
+    var rating_val = document.getElementById("rating_text" + no).value;
 
-    document.getElementById("fname_row" + no).innerHTML = fname_val;
-    document.getElementById("lname_row" + no).innerHTML = lname_val;
-    document.getElementById("anum_row" + no).innerHTML = anum_val;
+    document.getElementById("title_row" + no).innerHTML = title_val;
+    document.getElementById("genre_row" + no).innerHTML = genre_val;
+    document.getElementById("rating_row" + no).innerHTML = rating_val;
 
     document.getElementById("edit_button" + no).style.display = "block";
     document.getElementById("save_button" + no).style.display = "none";
@@ -33,17 +33,17 @@ function delete_row(no) {
 }
 
 function add_row() {
-    var fname = document.getElementById("fname").value;
-    var lname = document.getElementById("lname").value;
-    var anum = document.getElementById("anum").value;
+    var title = document.getElementById("title").value;
+    var genre = document.getElementById("genre").value;
+    var rating = document.getElementById("rating").value;
 
     var table = document.getElementById("data_table");
     var table_len = (table.rows.length) - 1;
-    var row = table.insertRow(table_len).outerHTML = "<tr id='row" + table_len + "'><td id='fname_row" + table_len + "'>" + fname + "</td><td id='lname_row" + table_len + "'>" + lname + "</td><td id='anum_row" + table_len + "'>" + anum + "</td><td><input type='button' id='edit_button" + table_len + "' value='Edit' class='edit' onclick='edit_row(" + table_len + ")'> <input type='button' id='save_button" + table_len + "' value='Save' class='save' onclick='save_row(" + table_len + ")'> <input type='button' value='Delete' class='delete' onclick='delete_row(" + table_len + ")'></td></tr>";
+    var row = table.insertRow(table_len).outerHTML = "<tr id='row" + table_len + "'><td id='title_row" + table_len + "'>" + title + "</td><td id='genre_row" + table_len + "'>" + genre + "</td><td id='rating_row" + table_len + "'>" + rating + "</td><td><input type='button' id='edit_button" + table_len + "' value='Edit' class='edit' onclick='edit_row(" + table_len + ")'> <input type='button' id='save_button" + table_len + "' value='Save' class='save' onclick='save_row(" + table_len + ")'> <input type='button' value='Delete' class='delete' onclick='delete_row(" + table_len + ")'></td></tr>";
 
-    document.getElementById("fname").value = "";
-    document.getElementById("lname").value = "";
-    document.getElementById("anum").value = "";
+    document.getElementById("title").value = "";
+    document.getElementById("genre").value = "";
+    document.getElementById("rating").value = "";
 }
 
 function getHttp(url) {
@@ -54,5 +54,5 @@ function getHttp(url) {
 }
 
 function listAllMovies() {
- getHttp('http://localhost:8080/Movie/api/account/getAllAccounts')
+ getHttp('http://localhost:8081/Movie/api/account/getAllAccounts')
 }
